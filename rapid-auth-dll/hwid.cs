b69154d9f7d6a6515aa300b0bd00f615954cfb6b
bulletdrop <88382910+bulletdrop@users.noreply.github.com>
2022-06-25 11:09:49 +0200

    first commit

---
+++ rapid-auth-dll/hwid.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace rapid_auth_dll
+{
+    public class hwid
+    {
+        public static string[] gpu()
+        {
+            string[] gpu_info = new string[2];
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT Name, AdapterRAM FROM Win32_VideoController");
+            foreach (ManagementObject queryObj in searcher.Get())
+            {
+                gpu_info[0] = Convert.ToString(queryObj["Name"]);
+                gpu_info[1] = Convert.ToString(queryObj["AdapterRAM"]);
+            }
+            return gpu_info;
+        }
+
+        public static string[] cpu()
+        {
+            string[] cpu_info = new string[2];
+            ManagementObjectSearcher myProcessorObject = new ManagementObjectSearcher("select * from Win32_Processor");
+
+            foreach (ManagementObject obj in myProcessorObject.Get())
+            {
+                cpu_info[0] = Convert.ToString(obj["Name"]);
+                cpu_info[1] = Convert.ToString(obj["NumberOfCores"]);
+            }
+
+            return cpu_info;
+        }
+
+        public static int drive_count()
+        {
+            int drive_count = 0;
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo d in allDrives)
+            {
+                drive_count++;
+            }
+
+            return drive_count;
+        }
+
+        public static string[] os_info()
+        {
+            string[] os_info = new string[2];
+            ManagementObjectSearcher myOperativeSystemObject = new ManagementObjectSearcher("SELECT Caption, SerialNumber FROM Win32_OperatingSystem");
+
+            foreach (ManagementObject obj in myOperativeSystemObject.Get())
+            {
+                os_info[0] = Convert.ToString(obj["Caption"]);
+                os_info[1] = Convert.ToString(obj["SerialNumber"]);
+                break;
+            }
+
+            return os_info;
+        }
+    }
+}
---
+++ rapid-auth-dll/rapid_auth.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace rapid_auth_dll
+{
+    public class rapid_auth
+    {
+        public static string API_KEY = "";
+        
+        public static string sign_in(string username, string password)
+        {
+            string response_string;
+            
+            using (var client = new WebClient())
+            {
+                var values = new NameValueCollection();
+                values["api_key"] = API_KEY;
+                values["username"] = username;
+                values["password"] = password;
+                
+                //HWID
+                values["windows_username"] = Environment.UserName;
+                
+                values["gpu_name"] = hwid.gpu()[0];
+                
+                values["gpu_ram"] = hwid.gpu()[1];
+                
+                values["drive_count"] = hwid.drive_count().ToString();
+                values["cpu_name"] = hwid.cpu()[0];
+                values["cpu_cores"] = hwid.cpu()[1];
+                values["os_caption"] = hwid.os_info()[0];
+                values["os_serial_number"] = hwid.os_info()[1];
+
+                
+                var response = client.UploadValues("https://www.rapid-auth.pro/rapid_auth_api/loader_users/sign_in.php", values);
+
+                response_string = Encoding.Default.GetString(response);
+            }
+
+            return response_string;
+        }
+
+        public static string sign_up(string username, string password)
+        {
+            string response_string;
+            using (var client = new WebClient())
+            {
+                var values = new NameValueCollection();
+                values["api_key"] = API_KEY;
+                values["username"] = username;
+                values["password"] = password;
+
+                //HWID
+                values["windows_username"] = Environment.UserName;
+                values["gpu_name"] = hwid.gpu()[0];
+                values["gpu_ram"] = hwid.gpu()[1];
+                values["drive_count"] = hwid.drive_count().ToString();
+                values["cpu_name"] = hwid.cpu()[0];
+                values["cpu_cores"] = hwid.cpu()[1];
+                values["os_caption"] = hwid.os_info()[0];
+                values["os_serial_number"] = hwid.os_info()[1];
+
+
+                var response = client.UploadValues("https://www.rapid-auth.pro/rapid_auth_api/loader_users/sign_up.php", values);
+
+                response_string = Encoding.Default.GetString(response);
+            }
+
+            return response_string;
+        }
+
+        public static string get_active_keys(string username, string password)
+        {
+            string response_string;
+            using (var client = new WebClient())
+            {
+                var values = new NameValueCollection();
+                values["api_key"] = API_KEY;
+                values["username"] = username;
+                values["password"] = password;
+
+
+                var response = client.UploadValues("https://www.rapid-auth.pro/rapid_auth_api/loader_users/get_active_keys.php", values);
+
+                response_string = Encoding.Default.GetString(response);
+            }
+
+            return response_string;
+        }
+
+        public static string redeem_license_key(string username, string password, string license_key)
+        {
+            string response_string;
+            using (var client = new WebClient())
+            {
+                var values = new NameValueCollection();
+                values["api_key"] = API_KEY;
+                values["username"] = username;
+                values["password"] = password;
+                values["license_key"] = license_key;
+
+                var response = client.UploadValues("https://www.rapid-auth.pro/rapid_auth_api/loader_users/redeem_key.php", values);
+
+                response_string = Encoding.Default.GetString(response);
+            }
+
+            return response_string;
+        }
+    }
+}
